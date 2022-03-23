b5730ba88801db72566893bdb027efee1e8740bf
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-03-23 14:21:34 +0700

    menambah loop

--- frontend/javascript/4-looping-cp/script-1.js
+++ frontend/javascript/4-looping-cp/script-1.js
@@ -15,3 +15,14 @@
 const counter = parseInt(prompt("Masukan jumlah bilangan yang ingin dicek: "));
 
 // TODO: answer here
+for(i = 1; i < counter + 1; i++){
+      if((i % 3 == 0) && (i % 5 == 0)){
+            console.log(i + " merupakan kelipatan 3 dan 5");
+      }else if(i % 3 == 0){
+            console.log(i + " merupakan kelipatan 3");
+      }else if(i % 5 == 0){
+            console.log(i + " merupakan kelipatan 5");
+      }else{
+            console.log(i);
+      }
+}
--- frontend/javascript/4-looping-cp/script-2.js
+++ frontend/javascript/4-looping-cp/script-2.js
@@ -13,3 +13,12 @@
 const n = parseInt(prompt("Masukan jumlah baris: "));
 
 // TODO: answer here
+
+let hasil = '';
+for (let i = 0; i < n; i++) {
+      for (let j = n; j > i; j--) {
+            hasil += '* ';
+      }
+      hasil += '\n';
+}
+console.log(hasil);
