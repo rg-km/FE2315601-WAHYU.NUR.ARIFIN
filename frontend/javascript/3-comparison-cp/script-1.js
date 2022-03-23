e25058806142c3f998f90cf9cad75d00a1b21422
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-03-23 10:45:52 +0700

    mengubah logika

--- frontend/javascript/3-comparison-cp/script-1.js
+++ frontend/javascript/3-comparison-cp/script-1.js
@@ -5,11 +5,16 @@ const number = parseInt(prompt("Masukan bilangan: "));
 
 
 // TODO: answer here
-if(number % 2 == 0){
-      console.log("genap");
-} else{
+if(number == 0){
+      console.log("nol");
+}else{
+      if(number % 2 == 0){
+            console.log("genap");
+      }else{ 
       console.log("ganjil");
+      }
 }
+
 
 // TODO: answer here
 
--- frontend/javascript/3-comparison-cp/script-2.js
+++ frontend/javascript/3-comparison-cp/script-2.js
@@ -3,9 +3,15 @@ const num1 = parseInt(prompt("Masukan bilangan pertama: "));
 const num2 = parseInt(prompt("Masukan bilangan kedua: "));
 const num3 = parseInt(prompt("Masukan bilangan ketiga: "));
 
-let largest = Math.max(num1, num2, num3);
+
 
 // TODO: answer here
-
+if(num1 > num2 && num1 > num3){
+      largest = num1;
+} else if (num2 > num3){
+      largest = num2;
+} else {
+      largest = num3;
+}
 // Menampilkan hasil
 console.log("Bilangan terbesar adalah " + largest);
