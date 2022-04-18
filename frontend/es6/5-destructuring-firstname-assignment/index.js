e03a5882a9a5c3793b5ef3fb4ca76042fd1d178f
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-04-18 21:01:45 +0700

    add-destructure-fname

--- frontend/es6/5-destructuring-firstname-assignment/index.js
+++ frontend/es6/5-destructuring-firstname-assignment/index.js
@@ -2,6 +2,8 @@
  */
 const getUserFirstName = (user) => {
   // TODO: answer here
+  const { name: firstName } = user;
+  return firstName;
 };
 
 console.log(getUserFirstName({ name: "John", email: "john@example.com" }));
