d863c893306be1d32e1c0e1ca704845202b1f399
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-04-18 21:13:28 +0700

    add-reduce-max

--- frontend/es6/3-functional-programming-reduce-max-assignment/index.js
+++ frontend/es6/3-functional-programming-reduce-max-assignment/index.js
@@ -9,6 +9,16 @@
 
 const maxFromArray = (numbers) => {
   // TODO: answer here
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+
+    if (numbers[i] > max) {
+      max = numbers[i];
+      
+    } else {
+      max = max;
+    }
+  }
   return max;
 };
 
