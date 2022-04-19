8afd6b242718f4dfbcc960a5803e87f8cb2994aa
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-04-19 10:36:39 +0700

    add-callback-cp

--- frontend/javascript/09-callback-cp/index.js
+++ frontend/javascript/09-callback-cp/index.js
@@ -4,11 +4,20 @@
 // contoh: hehe => hheehhee
 function doubleChars(str) {
   // TODO: answer here
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += str[i] + str[i];
+  }
+  return result;
 }
 
 // function ini mengulang pengaplikasian callback pada str sejumlah num
 function repeat(str, num, cb) {
   // TODO: answer here
+  for (let i = 0; i < num; i++) {
+    str = cb(str);
+  }
+  return str;
 }
 
 console.log(repeat("triple", 2, doubleChars)); //  ttttrrrriiiipppplllleeee
