69c2d6198b32aaa820adfde93488746c89d62f2d
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-04-24 20:27:49 +0700

    add-hashmap-2

--- frontend/data-structure/hashmap/2-hashmap-cp/index.js
+++ frontend/data-structure/hashmap/2-hashmap-cp/index.js
@@ -17,7 +17,34 @@
 // Explanation: Jika ditata, "apple" dan "paddle" memiliki huruf-huruf yang berbeda
 
 function anagramChecker(str1, str2) {
-    return true; // TODO: replace this
+    // return true; // TODO: replace this
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    let count1 = {};
+    let count2 = {};
+    for (let i = 0; i < str1.length; i++) {
+        if(count1[str1[i]]) {
+            count1[str1[i]]++;
+        } else {
+            count1[str1[i]] = 1;
+        }
+    }
+    for (let i = 0; i < str2.length; i++) {
+        if(count2[str2[i]]) {
+            count2[str2[i]]++;
+        } else {
+            count2[str2[i]] = 1;
+        }
+    }
+    for (let key in count1) {
+        if (count1[key] !== count2[key]) {
+            return false;
+        }
+    }
+    return true;
+    
+
 }
 
 console.log(anagramChecker("keen", "knee"));
