dc7c22ca725b548975fb4b2c10f0ab08e038e97a
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-04-27 08:34:13 +0700

    add-stack-4

--- frontend/data-structure/stack/4-peek-cp/index.js
+++ frontend/data-structure/stack/4-peek-cp/index.js
@@ -3,17 +3,38 @@
 module.exports = class Stack {
     constructor() {
         // TODO: answer here
+        this.data = []
+        this.size = 10
+        this.top = -1
     }
 
     push(elemen) {
         // TODO: answer here
+        if (this.data.length == this.size) {
+            throw "stack overflow"
+        } else {
+            this.top += 1
+            return this.data.push(elemen)
+        }
     }
 
     pop() {
         // TODO: answer here
+        if (this.top == -1) {
+            throw "stack underflow"
+        } else {
+            let pop = this.data.pop()
+            this.top -= 1
+            return pop
+        }
     }
 
     peek() {
         // TODO: answer here
+        if (this.top == -1) {
+            throw "stack is empty"
+        } else {
+            return this.data[this.top]
+        }
     }
 }
