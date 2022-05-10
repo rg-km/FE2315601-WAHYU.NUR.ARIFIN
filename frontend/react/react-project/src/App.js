349f1c186c9f803642f14dd12610c96e301e7528
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-05-10 07:31:29 +0700

    add-update-project-1

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -3,6 +3,20 @@ import "./App.css"
 
 function App() {
   // TODO: answer here
+  return (
+    <div className="App">  
+      <header className="App-header">
+        <h1 aria-label="App Title">Instagram</h1>
+      </header>
+
+      <div className="App-container" aria-label="App">
+        <div className="App-content">
+          <h2>Post</h2>
+        </div>
+      </div>
+    </div>
+
+  )
 }
 
 export default App
