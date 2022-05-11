f04e71ff27036059c57bd24d0109734eeebf1a5b
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-05-11 07:34:18 +0700

    update-navbar

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -4,19 +4,24 @@ import "./App.css"
 function App() {
   // TODO: answer here
   return (
-    <div className="App">  
-      <header className="App-header">
-        <h1 aria-label="App Title">Instagram</h1>
-      </header>
+    <div>
+      <nav className="navbar" aria-label="Navbar">
+        <h2 className="navbar-brand" href="/" data-id="brand" aria-label="App Title">
+          Instagram
+        </h2>
+        <img className="brand-image" aria-label="App Logo" src="../logo512.png"></img>
+      </nav>
 
-      <div className="App-container" aria-label="App">
-        <div className="App-content">
-          <h2>Post</h2>
-        </div>
+      <div className="container" aria-label="App">
+        <button className="like-btn" aria-label="Like Button">Like Button</button>
+        <button className="dislike-btn" aria-aria-label="Dislike Button">Dislike Button</button>
+
       </div>
-    </div>
 
-  )
+    </div>
+    
+
+  );
 }
 
 export default App
