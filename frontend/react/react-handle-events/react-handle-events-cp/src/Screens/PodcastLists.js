c87d178e4d32d27a0fdcd8a0360fdea09208242e
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-05-17 11:58:30 +0700

    add-handle-cp

--- frontend/react/react-handle-events/react-handle-events-cp/src/Screens/PodcastLists.js
+++ frontend/react/react-handle-events/react-handle-events-cp/src/Screens/PodcastLists.js
@@ -11,39 +11,49 @@ const PodcastLists = () => {
   const [podcastList, setPodcastList] = useState([]);
   const [showFormModal, setShowFormModal] = useState(false);
   const [formModalType, setFormModalType] = useState("ADD");
-  const [podcastId, setPodcastId] = useState(0)
+  const [podcastId, setPodcastId] = useState(0);
 
   const getPodcastListData = async () => {
     // TODO: answer here
+    const listPodcast = await axios.get(Constants.API_URL);
+    setPodcastList(listPodcast.data);
   };
 
   useEffect(() => {
     // TODO: answer here
+    getPodcastListData();
   }, []);
 
   return (
     <div>
       <Header headerText={"My Top Podcasts"} />
       <div>
-        <Button
-          style={{ marginTop: 10, marginLeft: 10, alignSelf: "flex-end" }}
-          variant="primary"
-          onClick={() => setShowFormModal(true)}
-        >
-          + Add Podcasts
+        <Button 
+          style={{ marginTop: 10, marginLeft: 10, alignSelf: "flex-end" }} 
+          variant="primary" 
+          onClick={() => setShowFormModal(true)}>
+            + Add Podcasts
         </Button>
-        {podcastList.map((item) => {
-          // TODO: answer here
-        })}
+        {podcastList.length > 0 &&
+          podcastList.map((item, index) => {
+            // TODO: answer here
+            return <PodcastListItem 
+            key={index} 
+            id={item.id} 
+            podcastItem={item} 
+            setFormModalType={setFormModalType} 
+            setShowFormModal={setShowFormModal} 
+            setPodcastId={setPodcastId} />;
+          })}
       </div>
-      <PodcastFormModal
-        podcastList={podcastList}
-        setPodcastList={setPodcastList}
-        showFormModal={showFormModal}
-        setShowFormModal={setShowFormModal}
-        setFormModalType={setFormModalType}
-        formModalType={formModalType}
-        podcastId={podcastId}
+      <PodcastFormModal 
+        podcastList={podcastList} 
+        setPodcastList={setPodcastList} 
+        showFormModal={showFormModal} 
+        setShowFormModal={setShowFormModal} 
+        setFormModalType={setFormModalType} 
+        formModalType={formModalType} 
+        podcastId={podcastId} 
       />
     </div>
   );
