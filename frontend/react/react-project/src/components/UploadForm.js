262024dfe28812b16341ebb06f8d8a746a289bc8
wahyunurarifin <wahyunurarifin70@gmail.com>
2022-05-18 08:21:13 +0700

    add-uploadform

--- frontend/react/react-project/src/components/UploadForm.js
+++ frontend/react/react-project/src/components/UploadForm.js
@@ -1,4 +1,38 @@
-// TODO: answer here
+import React,{useState} from "react";
+import Axios from "axios";
+
 export default function UploadForm({onSubmit}) {
-  // TODO: answer here
+  const url="https://rg-km.riegan.my.id/api/post/create"
+  const [data, setData] = useState({
+    caption:"Hello World",
+    image: null
+  });
+
+  function handleChange(e) {
+    const newdata = { ...data };
+    newdata[e.target.name] = e.target.value;
+    setData(newdata);
+  }
+
+  function submit(e){
+    e.preventDefault();
+    Axios.post(url,{
+      caption:data.caption,
+      image:data.image
+    })
+    .then(res => {
+      onSubmit(res.data);
+    })
+    .catch(err => console.log(err));
+  }
+  
+  return (
+    <div className="upload-form" aria-label="Upload Form">
+      <form onSubmit = {(e)=> submit(e)} >
+        <input onChange={(e)=>handleChange(e)} id="caption" value={data.caption} type="text" aria-label="Caption Input" />
+        <input onChange={(e)=>handleChange(e)} id="image" value={data.image} type="file" accept="image/png, image/jpg, image/gif" aria-label="Image Input" />
+        <button type="submit" aria-label="Submit Button" >Submit</button>
+      </form>
+    </div>
+  )
 }
